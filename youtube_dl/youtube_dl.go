79db805df168834ac618c7e2573b20fa77565ab7
Abidul Wahab Ramadan <abidul.rmdn@gmail.com>
2017-03-08 17:17:12 +0800

    go lang library for loading videos in the wanted folder

---
+++ youtube_dl/youtube_dl.go
@@ -0,0 +1,19 @@
+package youtube_dl
+
+import "os/exec"
+
+// Youtube-dl SRT Downloader
+type YoutubeDl struct {
+	Path string
+}
+
+func (youtubedl YoutubeDl) DownloadVideo(id string) error{
+	filename := "\"" + youtubedl.Path + "/" + id + ".srt\""
+	commandParams := " --write-auto-sub --skip-download -o " + filename + " -- " + id
+	commandName := "youtube-dl"
+	command := commandName + " " + commandParams
+	cmd := exec.Command("bash", "-c", command)
+	err := cmd.Run() // waits until the commands runs and finishes
+	return err
+}
+
