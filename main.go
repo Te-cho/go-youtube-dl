303c088f9a91be20faced2f45be6212823aaa125
Sanjeev Kumar <sanjeev@outlook.in>
2023-12-27 22:13:42 +0000

    support embed thumbnail in downloaded audio and refactor

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"flag"
 	"fmt"
+	"net/url"
 	"os"
 	"log"
 	"os/exec"
@@ -13,7 +14,6 @@ import (
 )
 
 
-const BASE_URL = "https://www.youtube.com/watch?v="
 const CMD = "youtube-dl"
 const BEST_VIDEO_FORMAT = "18"
 const BEST_AUDIO_FORMAT = "140"
@@ -25,9 +25,88 @@ type YoutubeDl struct {
 	Format string
 }
 
-func run(commandParams string) error {
-	command :=  CMD + commandParams
-	cmd := exec.Command("bash", "-c", command)
+func extractVideoID(videoURL string) (string, error) {
+	parsedURL, err := url.Parse(videoURL)
+	if err != nil {
+		return "", err
+	}
+
+	queryParams := parsedURL.Query()
+	videoID := queryParams.Get("v")
+
+	return videoID, nil
+}
+
+
+func (youtubedl YoutubeDl) DownloadSubs(url string) error {
+	videoID, error := extractVideoID(url)
+	if error != nil {
+		fmt.Fprintln(os.Stderr, error)
+		return error
+	}
+
+	cmdArgs := []string{
+		CMD,
+		"--write-auto-sub",
+		"--sub-lang",
+		"en",
+		"--output", youtubedl.Path + videoID + ".srt",
+		"--skip-download",
+		url,
+	}
+
+	err := run(cmdArgs)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return err
+	}
+
+	return nil
+}
+
+func (youtubedl YoutubeDl) DownloadAudio(url string) (error) {
+	// Escape and quote the path to handle spaces or special characters
+	escapedPath := "\"" + youtubedl.Path + "%(title)s-%(id)s.%(ext)s\""
+
+	cmdArgs := []string{
+		CMD,
+		"--format", youtubedl.Format,
+		"--extract-audio",
+		"--audio-format", "m4a",
+		"--audio-quality", "192",
+		"--output", escapedPath,
+		"--write-thumbnail",
+		"--embed-thumbnail",
+		url,
+	}
+
+	err := run(cmdArgs)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return err
+	}
+	return nil
+}
+
+func (youtubedl YoutubeDl) DownloadVideo(videoURL string) error {
+	sep := string(filepath.Separator)
+	// Escape and quote the path to handle spaces or special characters
+	escapedPath := "\"" + youtubedl.Path + sep + "%(title)s-%(id)s.%(ext)s\""
+
+	cmdArgs := []string{
+		CMD,
+		"--format", youtubedl.Format,
+		"--output", escapedPath,
+		"--embed-thumbnail",
+		videoURL,
+	}
+
+	return run(cmdArgs)
+}
+
+func run(cmdArgs []string) error {
+	fmt.Println("Starting ...")
+	cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
 
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
@@ -63,107 +142,95 @@ func run(commandParams string) error {
 	return nil
 }
 
-func (youtubedl YoutubeDl) DownloadSubs(id string) error {
-	filename := "\"" + youtubedl.Path + "/" + id + ".srt\""
-	commandParams := " --write-auto-sub --skip-download --sub-lang en -o " + filename + " -- " + id
-	err := run(commandParams)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return err
-	}
-	return nil
-}
-
-func (youtubedl YoutubeDl) DownloadMedia(id string) (error) {
-	fmt.Println("Downloading ...")
-	// Escape and quote the path to handle spaces or special characters
-	escapedPath := "\"" + youtubedl.Path + "%(title)s-%(id)s.%(ext)s\""
-	commandParams := " --format " + youtubedl.Format + " -o " + escapedPath + " " + BASE_URL + id
-	err := run(commandParams)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return err
-	}
-	return nil
-}
-
-
 func main() {
+
+	var (
+		audioFlag bool
+		format string
+		output string
+		subsFlag bool
+		url string
+		videoFlag bool
+		ytdlFlag bool
+	)
+
     usr, err := user.Current()
     if err != nil {
         panic(err)
     }
-
-	path := filepath.Join("home", usr.Username, "Music")
+	sep := string(filepath.Separator)
+	path := filepath.Join(sep, "home", usr.Username, "Music")
 
 	// Parsing command line flags
-	yid := flag.String("yid", "", "ID of the youtube video to download subs")
-	// url := flag.String("url", "", "url of the youtube /audio/video to download")
-	s := flag.Bool("s", false, "Download subs from the youtube for the specified video id")
-	a := flag.Bool("a", false, "download audio.")
-	v := flag.Bool("v", false, "download video.")
-	f := flag.String("f", "", "Specific media format to download.")
-	p := flag.String("p", path, "Path where you want to download.")
-	y := flag.Bool("y", false, "Find where youtube-dl is installed.")
+	flag.StringVar(&url, "url", "", "url of the youtube /audio/video to download")
+	flag.BoolVar(&subsFlag, "s", false, "Download subs from the youtube for the specified video")
+	flag.BoolVar(&audioFlag, "a", false, "Download audio")
+	flag.BoolVar(&videoFlag, "v", false, "Download video")
+	flag.StringVar(&format, "f", "", "Specify video format")
+	flag.StringVar(&output, "o", path + sep, "Path where the video will be written to")
+	flag.BoolVar(&ytdlFlag, "y", false, "Find where youtube-dl is installed and its version")
 
 	flag.Parse()
 
-	if _, err := os.Stat(*p); os.IsNotExist(err) {
-		fmt.Printf("Error: The specified path '%s' does not exist.\n", *p)
+	if _, err := os.Stat(output); os.IsNotExist(err) {
+		fmt.Printf("Error: The specified path '%s' does not exist.\n", output)
 		os.Exit(1)
 	}
 
-	// If user did not provide id, show usage
-	if *yid == "" && !*y {
+	// If user did not provide youtube video url, show usage.
+	if url == "" && !ytdlFlag {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	ytdl := YoutubeDl{Path: *p}
+	ytdl := YoutubeDl{Path: output + sep}
 
 	switch {
-		case *y:
-			cmd := exec.Command("which", "youtube-dl")
-			output, err := cmd.CombinedOutput()
+		case ytdlFlag:
+			cmdp := exec.Command("which", "youtube-dl")
+			output, err := cmdp.CombinedOutput()
 			if err != nil {
 				fmt.Printf("Error executing command: %v\n", err)
 				return
 			}
 		
-			fmt.Printf("Output:\n%s\n", output)
-			path, err := exec.LookPath(CMD)
-			if err != nil {
-				fmt.Printf("Error executing command: %v\n", err)
-			}
-			fmt.Printf("path:\n%s\n", path)
+			fmt.Printf("youtube-dl is located at:\n%s\n", output)
 
-		case *a:
-			if *f != "" {
-				ytdl.Format = *f
+			cmdv := exec.Command("youtube-dl", "--version")
+			version, error := cmdv.CombinedOutput()
+			if error != nil {
+				fmt.Printf("Error executing command: %v\n", error)
+				return
+			}
+			fmt.Printf("Version:\n%s\n", version)
+
+		case audioFlag:
+			if format != "" {
+				ytdl.Format = format
 			} else {
 				ytdl.Format = BEST_AUDIO_FORMAT
 			}
-			err := ytdl.DownloadMedia(*yid)
+			err := ytdl.DownloadAudio(url)
 			if err != nil {
 				fmt.Fprintln(os.Stderr, err)
 				os.Exit(1)
 			}
 
-		case *v:
-			if *f != "" {
-				ytdl.Format = *f
+		case videoFlag:
+			if format != "" {
+				ytdl.Format = format
 			} else {
 				ytdl.Format = BEST_VIDEO_FORMAT
 			}
-			err := ytdl.DownloadMedia(*yid)
+			err := ytdl.DownloadVideo(url)
 			if err != nil {
 				fmt.Fprintln(os.Stderr, err)
 				os.Exit(1)
 			}
 
-		case *s:
+		case subsFlag:
 			fmt.Println("Downloading subs...")
-			err := ytdl.DownloadSubs(*yid)
+			err := ytdl.DownloadSubs(url)
 			if err != nil {
 				fmt.Println("Error downloading video.")
 			}
